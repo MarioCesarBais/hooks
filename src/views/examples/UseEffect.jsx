a8f75b76d31b100988a81cc8660b18c2ef23317d
Mario Cesar Bais <mariocfbais@gmail.com>
2022-02-20 20:41:57 -0300

    UseMemo

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -8,36 +8,43 @@ function calcFatorial(num) {
     if (n === 0) return 1
     return calcFatorial(n - 1) * n
 }
-
+// Ex 02
+function calcPar(num) {
+    const n = parseInt(num)
+    if (n % 2 === 0) return 'PAR'
+    else return 'ÍMPAR'
+}
+// Ex 01
 const UseEffect = (props) => {
     const [number, setNumber] = useState(1)
     const [fatorial, setFatorial] = useState(1)
+    const [ehPar, setEhPar] = useState(1)
 
-    useEffect(function() {
-        setFatorial(calcFatorial(number))
-    }, [number])
+    useEffect(function() { setEhPar(calcPar(number)) }, [number])
+
+    useEffect(function() { setFatorial(calcFatorial(number)) }, [number])
 
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
+                subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
             <SectionTitle title='Exercício #01'></SectionTitle>
             <div className="center">
                 <div>
                     <span className="text">Fatorial:</span>
-                    <span className="text red">{
-                        fatorial === -1 ? "Não existe!" : fatorial
-                    }</span>
+                    <span className="text red">{ fatorial === -1 ? "Não existe!" : fatorial }</span>
                 </div>
                 <input clasname='input' type="number" value={number}
                     onChange={ e => setNumber(e.target.value) } />
             </div>
-            <SectionTitle title='Exercício #02'></SectionTitle>
-            <div className="center">
-
-            </div>
+            <SectionTitle title='Exercício #02' />
+                <div className="center">
+                    <div>
+                        <span className="text">Status:</span>
+                        <span className="text red">{ ehPar }</span>
+                    </div>
+                </div>
         </div>
     )
 }
